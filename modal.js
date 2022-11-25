854a3ad682ed491010c7de7bba7bdd31bfe79de6
Shiffff <alexis.privat@live.fr>
2022-11-25 18:20:44 +0100

    Comment

--- modal.js
+++ modal.js
@@ -23,16 +23,13 @@ const inputfield = document.querySelector(".text-control");
 const modalBody = document.querySelector(".modal-body");
 const ConfirmationContainer = document.querySelector(".ConfirmationContainer");
 
-const form = document.querySelector("form"); // select form
-
-let readyToSent = [];
+const form = document.querySelector("form"); // Select the form
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // close modal event
 closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 closeBtnbottom.forEach((btn) => btn.addEventListener("click", closeModal));
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
@@ -42,8 +39,10 @@ function closeModal() {
   modalbg.style.display = "";
 }
 
-const regExpName = new RegExp(
-  "^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$", // regExp check the value from field
+let readyToSent = []; // Create empty array for stock each fields of form
+
+const regExpName = new RegExp( // Regexp for control the field of form
+  "^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$",
   "i"
 );
 const regExpEmail = new RegExp(
@@ -55,12 +54,14 @@ const regExpBirthdate = new RegExp(
   "^d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$"
 );
 const inputName = [
+  // Create an array of object for stock differents param of each field
   {
-    el: form.first,
+    el: form.first, //Select form field
     name: "Prénom",
+    // select the right message
     errorMessage:
       "Le prénom doit comporter au moins deux lettres (caractères spéciaux et chiffre non autorisé)",
-    regExp: regExpName,
+    regExp: regExpName, // Use the regexp
     listenerMethod: "change",
   },
   {
@@ -109,38 +110,44 @@ const inputName = [
     listenerMethod: "change",
     errorMessage: "",
   },
-]; // get every field on the form
-
-// ************************************************************************ First form's field 'Prénom'
+];
+// ***************************************************************************************************************
 
 const handleInput = (obj) => {
+  // create one function for control each field of form (exept Radio button) on "Change" listenerMethod
+
   obj.el.addEventListener(obj.listenerMethod, function () {
-    const errorMessage = obj.el.parentNode.querySelector(".errorMessage");
+    // Choice listener methode (change)
+    const errorMessage = obj.el.parentNode.querySelector(".errorMessage"); // select the parent element for select each error message field
     if (obj.regExp) {
-      const testFirst = obj.regExp.test(obj.el.value);
-      obj.valid = testFirst === true;
+      // If there is a regexp field on "[inputName]" object
+      const testFirst = obj.regExp.test(obj.el.value); // test with the right regexp
+      obj.valid = testFirst === true; // if the regexp OK
 
       if (obj.valid === true) {
-        obj.el.classList.remove("errorMessageName"); // setAttribute use to add a css rule // removeAttribute remove the css rule when it is there
-        errorMessage.innerText = ""; // remove text when it is there
-        return true; // and return true because the regExp pass
+        // create a new field valid on "[inputName]" object if the regexp OK
+        obj.el.classList.remove("errorMessageName"); // remove the errorMessage class if is it
+        errorMessage.innerText = ""; // and remove the message
+        return true;
       } else {
         // When the value doesn't pass the regexp
-        errorMessage.innerText = obj.errorMessage; // innerText use to add text in HTML
+        errorMessage.innerText = obj.errorMessage; // Write error message in the error class
 
-        obj.el.classList.add("errorMessageName"); // setAttribute use to add a css rule
-        return false; // the function return false because the regExp doesn't pass
+        obj.el.classList.add("errorMessageName"); // And add error class for create a red border
+        return false;
       }
     } else if (obj.checkBoxController === true) {
+      // if there isn't regexp field but checkBoxController field, check if is it checked and add error message add valid on object
       if (obj.el.checked) {
+        // there is the control only in checkbox 1
         obj.valid = true;
-
         errorMessage.innerText = "";
       } else {
         obj.valid = false;
         errorMessage.innerText = obj.errorMessage;
       }
     } else {
+      // there is the control for checkbox 2 but here, there isn't error message because this field isn't require
       if (obj.el.checked) {
         obj.valid = true;
       } else {
@@ -149,47 +156,31 @@ const handleInput = (obj) => {
     }
   });
 };
-
-inputName.forEach(handleInput);
-
+inputName.forEach(handleInput); // Call handleInput for each object of [inputName] array
+// ***************************************************************************************************************
 const handleSubmit = (obj) => {
-  const errorMessage = obj.el.parentNode.querySelector(".errorMessage");
+  // create one function for control each field of form  on "Submit" listenerMethod
+  const errorMessage = obj.el.parentNode.querySelector(".errorMessage"); // select the parent element for select each error message field
 
   if (obj.valid === true) {
-    errorMessage.innerText = "";
-    const newObj = { [obj.name]: obj.el.value };
-    readyToSent.push(newObj);
+    // If there is a field valid = true, on [inputname] array
+    errorMessage.innerText = ""; // supr the error message
+    const newObj = { [obj.name]: obj.el.value }; // and create a new object with name and value of field
+    readyToSent.push(newObj); // and send each object to [readyToSent] array
   } else {
-    errorMessage.innerText = obj.errorMessage;
+    errorMessage.innerText = obj.errorMessage; // otherwise write the right error message
   }
-};
-
-form.addEventListener("submit", function (e) {
-  // launch the function when the user click on the submit button
-  e.preventDefault();
-  inputName.forEach(handleSubmit);
-  checkRadioValue(form.location);
-  if (readyToSent.length > 6) {
-    launchModalConfirmation();
-    console.log(readyToSent);
-  } else {
-    readyToSent = [];
-  }
-});
-
-const launchModalConfirmation = () => {
-  // function for disable the form and replace it to the confirmation message
-  modalBody.style.display = "none";
-  ConfirmationContainer.style.display = "block";
 };
 
 const checkRadioValue = (radioArray) => {
+  //this function controle just Radio button 'location'
   const errorRadioMessage = document.querySelector(".ErrorCheckbox");
 
   if (radioArray.value.length > 1) {
+    // if one of radio button is selected return the value to Readytosent array
     errorRadioMessage.innerText = "";
     const newObj = { location: radioArray.value };
-    readyToSent.push(newObj);
+    readyToSent.push(newObj); // send to the array
     return radioArray.value;
   } else {
     errorRadioMessage.innerText = "Vous devez choisir une option.";
@@ -197,277 +188,23 @@ const checkRadioValue = (radioArray) => {
   }
 };
 
-/*
-
-const checkRadioValue = () => {
-  const getSelectedValue = document.querySelector(
-    'input[name="location"]:checked' // select every name of 'location' to found each radio button selected
-  );
-  if (getSelectedValue != null) {
-    ErrorCheckbox.innerText = "";
-    return true;
+form.addEventListener("submit", function (e) {
+  // On submit
+  e.preventDefault(); // don't refresh the page
+  inputName.forEach(handleSubmit); // And call the handleSubmit function for each element of inputname array
+  checkRadioValue(form.location); // Call the function for control radio button 'location'
+  if (readyToSent.length > 6) {
+    // If the object contain 7 or more object, console log "readyToSent" array and valid the form (call modal confirmation)
+    launchModalConfirmation();
+    console.log(readyToSent);
   } else {
-    ErrorCheckbox.innerText = "Vous devez choisir une option.";
-    return false;
-  }
-};
-
-const checkRadio = () => {
-  const getSelectedValue = document.querySelector(
-    'input[name="location"]:checked'
-  );
-  if (getSelectedValue != null) {
-    return getSelectedValue.value; // return the value of selected radio button
-  }
-};
-
-
-  validecheckbox1();
-  valideBirthdate();
-  valideFirstOnSubmit();
-  valideLastOnSubmit();
-  valideEmailOnSubmit();
-  valideQuantityOnSubmit();
-  checkRadioValue();
-
-  if (
-    valideFirst(inputFirst) && // last control, for test if every function return 'true'
-    valideFirstOnSubmit() &&
-    valideLast(inputLast) &&
-    valideLastOnSubmit() &&
-    valideEmail(inputEmail) &&
-    valideEmailOnSubmit() &&
-    valideBirthdate(inputBirthdate) &&
-    valideQuantity(inputQuantity) &&
-    valideQuantityOnSubmit() &&
-    validecheckbox1() &&
-    checkRadioValue()
-  ) {
-    const readyToSend = {
-      // create an object with each field of form
-      firstName: inputFirst.value,
-      lastName: inputLast.value,
-      email: inputEmail.value,
-      birthDate: inputBirthdate.value,
-      quantityTournament: inputQuantity.value,
-      conditionChecked: inputCheckbox1.checked,
-      eventChecked: inputCheckbox2.checked,
-      location: checkRadio(),
-    };
-    console.log(readyToSend);
-    launchModalConfirmation(); // launch the function for change the form to confirmation message
-  } else {
-    console.log("nok");
+    readyToSent = []; // otherwise clean the array
   }
 });
-  */
+// ***************************************************************************************************************
 
-// test
-
-/*
-inputFirst.addEventListener("change", function () {
-  // addEventListener call a function when the user modifies the element's value
-  handleInput(this);
-});
-const valideQuantityOnSubmit = function () {
-  if (inputQuantity.value) {
-    ErrorQuantity.innerText = "";
-    document
-      .querySelector("#quantity")
-      .removeAttribute("style", "border:2px solid #ff4e5f;");
-
-    return true;
-  } else {
-    ErrorQuantity.innerText = "Veuillez entrer un nombre.";
-    document
-      .querySelector("#quantity")
-      .setAttribute("style", "border:2px solid #ff4e5f;");
-
-    return false;
-  }
+const launchModalConfirmation = () => {
+  // Show the modal confirmation
+  modalBody.style.display = "none";
+  ConfirmationContainer.style.display = "block";
 };
-
-
-const valideFirst = function (inputFirst) {
-  // the function called by addEventListener
-
-  const testFirst = firstRegExp.test(inputFirst.value); // test the value with regexp
-  if (testFirst == false) {
-    // When the value doesn't pass the regexp
-    ErrorFirst.innerText = // innerText use to add text in HTML
-      "Le prénom doit comporter au moins deux lettres (caractères spéciaux et chiffre non autorisé)";
-    document
-      .querySelector("#first")
-      .setAttribute("style", "border:2px solid #ff4e5f;"); // setAttribute use to add a css rule
-    return false; // the function return false because the regExp doesn't pass
-  } else {
-    document
-      .querySelector("#first")
-      .removeAttribute("style", "border:2px solid #ff4e5f;"); // removeAttribute remove the css rule when it is there
-    ErrorFirst.innerText = ""; // remove text when it is there
-    return true; // and return true because the regExp pass
-  }
-};
-const valideFirstOnSubmit = function () {
-  // That function is call only when the user click on the submit button
-  if (inputFirst.value.length < 1) {
-    // Second check to control if the value isn't empty
-    ErrorFirst.innerText =
-      "Le prénom doit comporter au moins deux lettres (caractères spéciaux et chiffre non autorisé)";
-
-    return false;
-  } else {
-    ErrorFirst.innerText = "";
-    return true;
-  }
-};
-// ************************************************************************ Second form's field 'Nom'
-
-inputLast.addEventListener("change", function () {
-  valideLast(this);
-});
-
-const valideLast = function (inputLast) {
-  const testLast = firstRegExp.test(inputLast.value);
-  if (testLast == false) {
-    ErrorLast.innerText =
-      "Le nom doit comporter au moins deux lettres (caractères spéciaux et chiffre non autorisé)";
-    document
-      .querySelector("#last")
-      .setAttribute("style", "border:2px solid #ff4e5f;");
-
-    return false;
-  } else {
-    ErrorLast.innerText = "";
-    document
-      .querySelector("#last")
-      .removeAttribute("style", "border:2px solid #ff4e5f;");
-    return true;
-  }
-};
-
-const valideLastOnSubmit = function () {
-  if (inputLast.value.length < 1) {
-    ErrorLast.innerText =
-      "Le nom doit comporter au moins deux lettres (caractères spéciaux et chiffre non autorisé)";
-    document
-      .querySelector("#last")
-      .setAttribute("style", "border:2px solid #ff4e5f;");
-    return false;
-  } else {
-    ErrorLast.innerText = "";
-    document
-      .querySelector("#last")
-      .removeAttribute("style", "border:2px solid #ff4e5f;");
-
-    return true;
-  }
-};
-
-// ************************************************************************ Third form's field 'Email'
-
-inputEmail.addEventListener("change", function () {
-  valideEmail(this);
-});
-
-const valideEmail = function (inputEmail) {
-  const EmailRegExp = new RegExp(
-    "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$",
-    "g"
-  );
-  const testEmail = EmailRegExp.test(inputEmail.value);
-  if (testEmail == false) {
-    ErrorEmail.innerText =
-      "Veuillez entrer une adresse électronique est valide.";
-    document
-      .querySelector("#email")
-      .setAttribute("style", "border:2px solid #ff4e5f;");
-
-    return false;
-  } else {
-    ErrorEmail.innerText = "";
-    document
-      .querySelector("#email")
-      .removeAttribute("style", "border:2px solid #ff4e5f;");
-
-    return true;
-  }
-};
-inputBirthdate.addEventListener("change", function () {
-  valideBirthdate(this);
-});
-
-const valideEmailOnSubmit = function () {
-  if (inputEmail.value.length < 1) {
-    ErrorEmail.innerText =
-      "Veuillez entrer une adresse électronique est valide.";
-    document
-      .querySelector("#email")
-      .setAttribute("style", "border:2px solid #ff4e5f;");
-
-    return false;
-  } else {
-    ErrorEmail.innerText = "";
-    document
-      .querySelector("#email")
-      .removeAttribute("style", "border:2px solid #ff4e5f;");
-
-    return true;
-  }
-};
-
-inputQuantity.addEventListener("change", function () {
-  valideQuantity(this);
-});
-const valideQuantity = function (inputQuantity) {
-  const QuantityRegExp = new RegExp("^[0-9]");
-  const testQuantity = QuantityRegExp.test(inputQuantity.value);
-  if (testQuantity == false) {
-    ErrorQuantity.innerText = "Veuillez entrer un nombre.";
-    document
-      .querySelector("#quantity")
-      .setAttribute("style", "border:2px solid #ff4e5f;");
-
-    return false;
-  } else {
-    ErrorQuantity.innerText = "";
-    document
-      .querySelector("#quantity")
-      .removeAttribute("style", "border:2px solid #ff4e5f;");
-
-    return true;
-  }
-};
-
-const valideBirthdate = function () {
-  if (inputBirthdate.value) {
-    ErrorBirthdate.innerText = "";
-    document
-      .querySelector("#birthdate")
-      .removeAttribute("style", "border:2px solid #ff4e5f;");
-
-    return true;
-  } else {
-    ErrorBirthdate.innerText = "Vous devez entrer votre date de naissance.";
-    document
-      .querySelector("#birthdate")
-      .setAttribute("style", "border:2px solid #ff4e5f;");
-
-    return false;
-  }
-};
-const validecheckbox1 = function () {
-  if (inputCheckbox1.checked) {
-    // .checked for verify if the case is check
-    ErrorCheckbox1.innerText = "";
-
-    return true;
-  } else {
-    ErrorCheckbox1.innerText =
-      "Vous devez vérifier que vous acceptez les termes et conditions.";
-    return false;
-  }
-};
-
-*/
-// ************************************************************************ Fourth form's field 'Date de naissance'
